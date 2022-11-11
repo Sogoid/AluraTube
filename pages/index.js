7ad48ec4dfe7b2b05d598880e3df795d98672fe6
Sogoid <diogos_ribeiro@outlook.com>
2022-11-11 01:17:30 -0300

    Ajuste de layout

--- pages/index.js
+++ pages/index.js
@@ -45,6 +45,9 @@ export default function HomePage() {
                             </div>
                         </div>
                     </div>
+                    <div>
+                        <p className={styles.linhafooter}></p>
+                    </div>
                     <Footer />
                 </section>
             </div>
